9e4532663ecbc30fbd1c43d7047a868afc864cd3
alper.kacar.3@gmail.com <alper.kacar.3@gmail.com>
2023-02-16 22:39:41 +0300

    Merge branch 'master' of https://github.com/chivasper/gelecegim-app
    # Conflicts:
    #	src/Components/Header.js
    #	src/Pages/membership/Login.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,24 +1,70 @@
 import React from "react";
+import { Link} from "react-router-dom";
 import styled from "styled-components";
 import logosrc from "../images/Gelecegim_Logo.png";
-import Ara from "./Search";
-import headerCss from "./ComponentCss/HeaderCss";
+import { IoSearchSharp } from "react-icons/io5";
+const Head = styled.div`
+  width: 100%;
+  height: 57.9px;
 
+  background-color: #3f475f;
+`;
+const Navbar = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  list-style: none;
+  justify-content: space-around;
+`;
+
+const Search = styled.input`
+  font-family: "Lucida Grande", "Lucida Sans", "Lucida Sans Unicode", Roboto,
+    sans-serif;
+  font-size: 11px;
+  width: 243px;
+  height: 26px;
+  border-radius: 3px;
+`;
+const Li = styled.li`
+  color: white;
+  padding-left: 10px;
+  display: flex;
+  align-items: center;
+  float: left;
+`;
+const Logom = styled.img`
+  width: 160px;
+  height: 34px;
+`;
+const Searchicon = styled.div`
+  margin: 0;
+  padding: 0;
+  float: left;
+  display: flex;
+  position: relative;
+  right:310px;
+`;
 function Header() {
   return (
-    <headerCss.Head>
-      <headerCss.Navbar>
-        <headerCss.Logom src={logosrc} alt="selam" />
+    <Head>
+      <Navbar>
+        <Logom src={logosrc} alt="selam" />
 
-        <Ara />
+        <Search
+          id="searchText"
+          placeholder="Kelime, ilan no veya kurum adı ile ara"
+        />
+        <Searchicon>
+          <IoSearchSharp />
+        </Searchicon>
 
         <ul>
-          <Li>Giriş Yap</Li>
+        <Link to="auth/login">Giriş Yap</Link>
 
-          <Li>Hesap Aç</Li>
+        <Link to="auth/signup">Hesap Aç</Link>
         </ul>
-      </headerCss.Navbar>
-    </headerCss.Head>
+      </Navbar>
+    </Head>
   );
 }
 export default Header;
