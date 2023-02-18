df92a212fa1f440499850f083515b3c022a389ab
alper.kacar.3@gmail.com <alper.kacar.3@gmail.com>
2023-02-18 22:18:58 +0300

    redux tolkit yükledim + login signup yapıldı

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -5,14 +5,7 @@ import { IoSearchSharp } from "react-icons/io5";
 function Ara() {
   return (
     <searchCss.Searchdiv>
-      <searchCss.Search
-        id="searchText"
-        placeholder=" Kelime, ilan no veya kurum adı ile ara"
-      />
 
-      <searchCss.Searchicon>
-        <IoSearchSharp />
-      </searchCss.Searchicon>
     </searchCss.Searchdiv>
   );
 }
