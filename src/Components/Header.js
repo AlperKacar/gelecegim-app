09e309247590b3314009252f4d43ad19be9635e5
alper.kacar.3@gmail.com <alper.kacar.3@gmail.com>
2023-02-19 16:41:16 +0300

    header düzeltildi css bozuldu

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,18 @@
 import React from "react";
 import { Link} from "react-router-dom";
 import logosrc from "../images/revize3.png";
-import HeaderCss from "./ComponentCss/HeaderCss"
+import {Head} from "./ComponentCss/HeaderCss"
 import Search from "./Search"
-import { setUser } from "../store/auth";
 import { useDispatch } from "react-redux";
+import { setUser } from "../store/auth";
 import {useNavigate, useLocation} from "react-router-dom"
-
-
-
+import { useState } from "react";
 
 function Header() {
-
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const location = useLocation()
-
+  const [state,setState] = useState(false)
 
   const logoutHandel = () => {
   dispatch(setUser(false))
@@ -24,24 +21,37 @@ function Header() {
   })
 } 
 
-
   return (
-    <HeaderCss.Head>
-      <HeaderCss.Navbar>
-        
-        <HeaderCss.Logom src={logosrc} alt="selam" />
-        
+    <Head>
+    <div className="Navbar">
+    <img className="Logom" src={logosrc} alt="selam" />
         <Search/>
-        <ul>
-        <Link className="navLink" to="auth/login">Giriş Yap</Link>
+        {
+          //giriş çıkış görseli 
+        state 
+        ? 
+        //dive display flex   aline item center 
+        //çıkış butonu iconlu menü profile git profili düzenle çıkış yap
+        <div >
+          
+          <ul>
+            <Link className="navLink pr-2" to="auth/login">Giriş Yap</Link>
+            <Link className="navLink border"to="auth/signup">Hesap Aç</Link>
+          </ul> 
+          <button className="kurum-kayit" name="Kurum Kaydı" href="/">Kurum Kaydı</button>
+        </div>
+        : 
+        <div onClick={() => setState(state =>!state)}>
+        <button onClick={logoutHandel} className="kurum-kayit" name="Kurum Kaydı" href="/">Çıkış</button>
 
-        <Link className="navLink"to="auth/signup">Hesap Aç</Link>
-        </ul>
-        <button onClick={logoutHandel}>çıkış yap</button>
-      </HeaderCss.Navbar>
-    </HeaderCss.Head>
+        </div>
+      }
+       
+      </div>
+    </Head>
     
     
   );
 }
+
 export default Header;
