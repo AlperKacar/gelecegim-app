1035b1b9f281d423a5853282bba03e5e39626d27
Ertnercr <ertan.eracar@gmail.com>
2023-02-19 15:33:29 +0300

    kurum kaydı butonu eklendi headerda biraz daha rötuş yapıldı

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,26 +1,27 @@
 import React from "react";
 import { Link} from "react-router-dom";
 import logosrc from "../images/revize3.png";
-import HeaderCss from "./ComponentCss/HeaderCss"
+import {Head} from "./ComponentCss/HeaderCss"
 import Search from "./Search"
 
 function Header() {
   return (
-    <HeaderCss.Head>
-      <HeaderCss.Navbar>
+    <Head>
+      <div className="Navbar">
         
-        <HeaderCss.Logom src={logosrc} alt="selam" />
+        <img className="Logom" src={logosrc} alt="selam" />
         
         <Search/>
         <ul>
-        <Link className="navLink" to="auth/login">Giriş Yap</Link>
-
-        <Link className="navLink"to="auth/signup">Hesap Aç</Link>
+        <Link className="navLink pr-2" to="auth/login">Giriş Yap</Link>
+        
+        <Link className="navLink border"to="auth/signup">Hesap Aç</Link>
         </ul>
-      </HeaderCss.Navbar>
-    </HeaderCss.Head>
+        <button className="kurum-kayit" name="Kurum Kaydı" href="/">Kurum Kaydı</button>
+      </div>
+    </Head>
     
     
   );
-}
+} 
 export default Header;
