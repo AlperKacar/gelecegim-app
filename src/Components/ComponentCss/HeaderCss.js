ec4c63bfd3eaa5b88397d56c6262fc955c0bfe84
Ertnercr <ertan.eracar@gmail.com>
2023-02-20 22:49:43 +0300

    açılır menü düzenlemeleri ant design eklendi açılır menü ayrı component oldu

--- src/Components/ComponentCss/HeaderCss.js
+++ src/Components/ComponentCss/HeaderCss.js
@@ -1,73 +1,26 @@
-
-import styled from "styled-components"
-
+import styled from "styled-components";
 
 export const Head = styled.div`
   width: 100%;
   height: 57.9px;
   background-color: #3f475f;
   display: grid;
-  
 
-  .Navbar{
-  width:1115px;
-  display: flex;
-  align-items: center;
-  list-style: none;
-  justify-content: space-between;
-  margin:0 auto;
-  
+  .Navbar {
+    width: 1115px;
+    display: flex;
+    align-items: center;
+    list-style: none;
+    justify-content: space-between;
+    margin: 0 auto;
   }
 
- .Logom{
-  background-color: yellow;
-  width: 160px;
-  height: 34px;
-  
- } 
+  .Logom {
+    background-color: yellow;
+    width: 160px;
+    height: 34px;
+  }
 
-.navLink{
-  font-size: 12px;
-  padding-left:3px;
-  text-decoration: none;
-  color: white;
-  
-}
 
-.border{
-  
-  padding-left:3px;
-  border-left: 1px solid gray;
-  
-  
-}
-.pr-2{
-  padding-right: 3px;
-}
-
-.kurum-kayit{
-  //margin-left:20px ben verdim sen düzenlersin
-  margin-left: 20px;
-  font-family: "Lucida Grande","Lucida Sans","Lucida Sans Unicode",Roboto,sans-serif;
-  font-size: 12px;
-  font-weight: bold;
-  cursor: pointer;
-  text-shadow: 1px 1px 0 #29619b;
-  box-shadow: inset 0 0 0 1px rgba(255,255,255,0.1), 0 2px 2px -1px rgba(0,0,0,0.15);
-  border-radius:2px;
-  color: white;
-  background-color:rgb(72,154,241);
-  width: 140px;
-  height: 35px;
-  border: none;
-}
-.kurum-kayit:hover{
-  background-color: #3388e3;
-}
 
 `;
-
-
-
-
-
---
+++ src/Components/Menu.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { DownOutlined } from "@ant-design/icons";
+import {Dropdown, Space } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { setUser } from "../store/auth";
+import { useNavigate, useLocation, Link } from "react-router-dom";
+import {KullaniciMenu} from "./ComponentCss/MenuCss"
+export function Menu() {
+  const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const logoutHandle = () => {
+    dispatch(setUser(false));
+    navigate(location.state?.return_url || "/", {
+      replace: true,
+    });
+  };
+  const items = [
+    {
+      label: <Link to="/profile">Profili Görüntüle</Link>,
+      key: "0",
+    },
+    {
+      label: <a href="https://www.aliyun.com">Profili Düzenle</a>,
+      key: "1",
+    },
+    {
+      type: "divider",
+    },
+    {
+      type: "divider",
+    },
+    {
+      label:<a className="alt-li" onClick={logoutHandle}>Çıkış Yap</a>,
+      key: "3",
+    },
+  ];
+  return (
+      <KullaniciMenu>
+
+    {user ? (
+        
+      <Dropdown className="alt-li"
+      menu={{items}}trigger={["click"]}>
+      <a onClick={(e) => e.preventDefault()}>
+        <Space className="namesurname">
+          Ad Soyad
+          <DownOutlined className="down-arrow" />
+        </Space>
+      </a>
+    </Dropdown>
+      
+       ) : (
+        <div className="ul-div">
+        <ul>
+          <Link className="navLink pr-10" to="auth/login">
+            Giriş Yap
+          </Link>
+          <Link className="navLink border pl-10" to="auth/signup">
+            Hesap Aç
+          </Link>
+        </ul>
+        <button className="kurum-kayit" name="Kurum Kaydı" href="/">
+          Kurum Kaydı
+        </button>
+      </div>
+    )}
+      </KullaniciMenu>
+  );
+}
